078cbd6b320de45acb725b08ad26f721f9edf1e3
Talari Teja <talari.teja@STLERI.COM>
2022-11-14 17:52:23 +0530

    written a small user input program

--- test.py
+++ test.py
@@ -1,2 +1,28 @@
-print("pushing to git")
-print("It is text file")
+lst = []
+i=1
+count = 5
+while i:
+    lst.append(input("Enter values : "))
+    i+=1
+    if len(lst) == count:
+        user_guide = input("Please enter'Y' to continue or press 'N' to stop : ")
+        if user_guide == 'Y':
+            pass
+            count+=5
+            print(lst)
+        else:
+            break
+print(lst)
+
+
+
+
+
+
+
+
+        
+
+
+
+        
