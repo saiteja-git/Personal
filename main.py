d1fb0c451e2e2fd584beb8e2d21dc13ddcfb0dd4
Talari Teja <talari.teja@STLERI.COM>
2022-11-14 15:46:36 +0530

    added sum function

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("its is main file")
+
+
+add1 = int(input("Enter first value : "))
+add2 = int(input("enter second value : "))
+
+
+
+def add_fun (add1, add2):
+
+    return add1 + add2
+
+print(add_fun(add1, add2)) 
